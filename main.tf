4dfcc70c7365e25067c55861ad0eeb74012687f2
neouser11 <adeb1@uncc.edu>
2023-04-12 18:52:42 -0400

    Cosmosdb deployment

--- main.tf
+++ main.tf
@@ -42,3 +42,42 @@ resource "azurerm_storage_account" "storage" {
 
   tags = local.tags
 }
+
+
+resource "azurerm_cosmosdb_account" "db" {
+  name                = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}db"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  offer_type          = "Standard"
+  kind                = "MongoDB"
+
+  enable_automatic_failover = true
+
+  capabilities {
+    name = "EnableAggregationPipeline"
+  }
+
+  capabilities {
+    name = "mongoEnableDocLevelTTL"
+  }
+
+  capabilities {
+    name = "MongoDBv3.4"
+  }
+
+  capabilities {
+    name = "EnableMongo"
+  }
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
+
+  geo_location {
+    location          = "eastus"
+    failover_priority = 1
+  }
+
+}
