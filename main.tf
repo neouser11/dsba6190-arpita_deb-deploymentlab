ba3bf1e8ee2532d148aa4d2804e6913393026b25
neouser11 <adeb1@uncc.edu>
2023-04-12 20:14:14 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -114,3 +114,48 @@ resource "azurerm_machine_learning_workspace" "rg" {
   }
 }
 
+
+resource "azurerm_data_factory" "azdf" {
+  name                = "azdf"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+
+//Firewall
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "test11vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "snet" {
+  name                 = "AzureFirewallSubnet1123"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "testpip11"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_firewall" "fwall" {
+  name                = "testfirewall11221"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku_name            = "AZFW_VNet"
+  sku_tier            = "Standard"
+
+  ip_configuration {
+    name                 = "configuration112"
+    subnet_id            = azurerm_subnet.snet.id
+    public_ip_address_id = azurerm_public_ip.pip.id
+  }
+}
