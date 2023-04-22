2c44acc16eee3d0d7ef7fa23f610a923d66f992f
neouser11 <adeb1@uncc.edu>
2023-04-22 18:25:46 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "azurerm_storage_account" "storage" {
 
 // Storage Account with HNS 
 resource "azurerm_storage_account" "storage2hns" {
-  name                     = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}st-hns"
+  name                     = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}sthns"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
