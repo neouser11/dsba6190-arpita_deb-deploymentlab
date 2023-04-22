23b036b7c19ccb7e4f25ea845dbfdedaa3381258
neouser11 <adeb1@uncc.edu>
2023-04-22 18:04:41 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 
-// Storage Account
+// Storage Account 1 For machine learning workspace
 
 resource "azurerm_storage_account" "storage" {
   name                     = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}st"
@@ -43,9 +43,21 @@ resource "azurerm_storage_account" "storage" {
   tags = local.tags
 }
 
+// Storage Account with HNS
+resource "azurerm_storage_account" "storage2hns" {
+  name                     = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}st-hns"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  is_hns_enabled           = true
 
+  tags = local.tags
+}
+
+//COSMOS db
 resource "azurerm_cosmosdb_account" "db" {
-  name                = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}db"
+  name                = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}cosmosdb"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   offer_type          = "Standard"
@@ -82,31 +94,31 @@ resource "azurerm_cosmosdb_account" "db" {
 
 }
 
-// Machine Learning 
+// Machine Learning Workspace
 
 data "azurerm_client_config" "current" {}
 
-resource "azurerm_application_insights" "rg" {
-  name                = "workspace-rg-ai"
+resource "azurerm_application_insights" "insights" {
+  name                = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}insights"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   application_type    = "web"
 }
 
-resource "azurerm_key_vault" "rg" {
-  name                = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}vt"
+resource "azurerm_key_vault" "kvt" {
+  name                = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}kvt"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = "premium"
 }
 
-resource "azurerm_machine_learning_workspace" "rg" {
-  name                    = "rg-workspace"
+resource "azurerm_machine_learning_workspace" "mlw" {
+  name                    = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}ml"
   location                = azurerm_resource_group.rg.location
   resource_group_name     = azurerm_resource_group.rg.name
-  application_insights_id = azurerm_application_insights.rg.id
-  key_vault_id            = azurerm_key_vault.rg.id
+  application_insights_id = azurerm_application_insights.insights.id
+  key_vault_id            = azurerm_key_vault.kvt.id
   storage_account_id      = azurerm_storage_account.storage.id
 
   identity {
@@ -114,6 +126,7 @@ resource "azurerm_machine_learning_workspace" "rg" {
   }
 }
 
+//Data Factory
 
 resource "azurerm_data_factory" "azdf" {
   name                = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}df"
@@ -132,7 +145,7 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_subnet" "snet" {
-  name                 = "AzureFirewallSubnet"
+  name                 = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}snet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
@@ -140,21 +153,21 @@ resource "azurerm_subnet" "snet" {
 
 resource "azurerm_public_ip" "azpip" {
   name                = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}pip"
-  location            = azurerm_resource_group.rg.location #?????
+  location            = azurerm_resource_group.rg.location 
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
   sku                 = "Standard"
 }
 
 resource "azurerm_firewall" "fwall" {
-  name                = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}fl"
+  name                = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}fwall"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   sku_name            = "AZFW_VNet"
   sku_tier            = "Standard"
 
   ip_configuration {
-    name                 = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}cg"
+    name                 = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}config"
     subnet_id            = azurerm_subnet.snet.id
     public_ip_address_id = azurerm_public_ip.azpip.id
   }
