650aa1fb1a81f70a5308c5b8f073c4e6d79bdeb4
neouser11 <adeb1@uncc.edu>
2023-04-22 18:31:49 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -145,7 +145,7 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_subnet" "snet" {
-  name                 = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}snet"
+  name                 = "AzureFirewallSubnet"#"${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}snet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
